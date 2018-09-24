0a0c9c120992391d54ff9e3f6c3c2ced5f0d5d47
williamsduarte <williamduarteoficial@gmail.com>
2018-09-24 17:24:43 -0400

    Rota Categoria Parentes

---
+++ app/Http/Controllers/Api/V1/Admin/CategoryParentController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\ApiController;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Services\Admin\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryParentController extends ApiController
+{
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var CategoryService
+     */
+    private $service;
+
+    /**
+     * CategoryParentController constructor.
+     * @param CategoryRepositoryInterface $repository
+     * @param CategoryService $service
+     */
+    public function __construct(CategoryRepositoryInterface $repository,
+                                CategoryService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        if (!$result = $this->repository->all(['parents'])) {
+            return $this->errorResponse('categories_not_found', 422);
+        }
+
+        return $this->showAll($result);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+
+        $validator = $this->service->validator($request->all());
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return $errors->toJson();
+        }
+
+        if (!$result = $this->repository->create($request->all())) {
+            return $this->errorResponse('category_not_created', 422);
+        }
+
+        return $this->successResponse($result);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+
+        if (!$result = $this->repository->findById($id, ['parents'])) {
+            return $this->errorResponse('category_not_found', 422);
+        }
+
+        return $this->showOne($result);
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+
+
+        $validator = $this->service->validator($request->all());
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return $errors->toJson();
+        }
+
+
+        if (!$result = $this->repository->findById($id)) {
+            return $this->errorResponse('category_not_found', 422);
+        }
+
+        if (!$result = $this->repository->update($id, $request->all())) {
+            return $this->errorResponse('category_not_updated', 422);
+        }
+
+        return $this->successResponse($result);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        if (!$result = $this->repository->findById($id)) {
+            return $this->errorResponse('category_not_found', 422);
+        }
+
+        if (!$result = $this->repository->delete($id)) {
+            return $this->errorResponse('category_not_removed', 422);
+        }
+
+        return $this->successResponse('category_removed');
+
+    }
+
+}
--- routes/admin/api-categories.php
+++ routes/admin/api-categories.php
@@ -12,3 +12,7 @@
 $this->resource('categories', 'Api\V1\Admin\CategoryController')->except([
     'create', 'edit'
 ]);
+
+$this->resource('categories.parents', 'Api\V1\Admin\CategoryParentController')->except([
+    'create', 'edit'
+]);
