8a41e7d49b36abbe930afa0029ba0c884033585a
williamsduarte <williamduarteoficial@gmail.com>
2018-10-02 17:58:44 -0400

    Refactory Seeder

--- database/seeds/AttributeSeeder.php
+++ database/seeds/AttributeSeeder.php
@@ -7,6 +7,180 @@ use App\Entities\AttributeGroup;
 
 class AttributeSeeder extends Seeder
 {
+
+    private $attributes = [
+
+        ['name' => 'Gênero', 'type' => AttributeGroup::GENRE],
+        ['name' => 'Produto com uma cor', 'type' => AttributeGroup::COLOR],
+        ['name' => 'Produto com duas cores', 'type' => AttributeGroup::COLOR],
+        ['name' => 'Tamanho de anel/aliança', 'type' => AttributeGroup::SIZE],
+        ['name' => 'Tamanho de calça', 'type' => AttributeGroup::SIZE],
+        ['name' => 'Tamanho de camisa/camiseta', 'type' => AttributeGroup::SIZE],
+        ['name' => 'Tamanho de tênis', 'type' => AttributeGroup::SIZE],
+
+    ];
+
+    private $genre = [
+        ['name' => 'Feminino'],
+        ['name' => 'Masculino'],
+    ];
+
+    private $color = [
+        ['name' => 'Creme', 'color' => '#FFF2CC'],
+        ['name' => 'Marrom', 'color' => '#B45F06'],
+        ['name' => 'Vermelho', 'color' => '#FF0000'],
+        ['name' => 'Cinza Claro', 'color' => '#CCCCCC'],
+        ['name' => 'Púrpura', 'color' => '#741B47'],
+        ['name' => 'Laranja', 'color' => '#FF9900'],
+        ['name' => 'Fúcsia', 'color' => '#E828FF'],
+        ['name' => 'Lilás', 'color' => '#8E7CC3'],
+        ['name' => 'Marrom amarelado', 'color' => '#F6B26B'],
+        ['name' => 'Ciano escuro', 'color' => '#76A5AF'],
+        ['name' => 'Verde escuro', 'color' => '#38761D'],
+        ['name' => 'Rosa', 'color' => '#F4CCCC'],
+        ['name' => 'Esmeralda', 'color' => '#93C47D'],
+        ['name' => 'Salmão', 'color' => '#EAD1DC'],
+        ['name' => 'Verde', 'color' => '#0C9800'],
+        ['name' => 'Fúcsia escuro', 'color' => '#C27BA0'],
+        ['name' => 'Dourado', 'color' => '#BF9000'],
+        ['name' => 'Lavanda', 'color' => '#D9D2E9'],
+        ['name' => 'Água', 'color' => '#83DDFF'],
+        ['name' => 'Azul Céu', 'color' => '#D0E0E3'],
+        ['name' => 'Preto', 'color' => '#000000'],
+        ['name' => 'Azul', 'color' => '#1717FF'],
+        ['name' => 'Azul claro', 'color' => '#CFE2F3'],
+        ['name' => 'Azul aço', 'color' => '#6FA8DC'],
+        ['name' => 'Vermelho escuro', 'color' => '#990000'],
+        ['name' => 'Azul escuro', 'color' => '#0B5394'],
+        ['name' => 'Índigo', 'color' => '#351C75'],
+        ['name' => 'Branco', 'color' => '#FFFFFF'],
+        ['name' => 'Branco Navajo', 'color' => '#FCE5CD'],
+        ['name' => 'Mocassim', 'color' => '#FFD966'],
+        ['name' => 'Azul petróleo', 'color' => '#134F5C'],
+        ['name' => 'Amarelo', 'color' => '#FFFF00'],
+        ['name' => 'Terracota', 'color' => '#E06666'],
+        ['name' => 'Violeta escuro', 'color' => '#7600FF'],
+        ['name' => 'Cinza', 'color' => '#666666'],
+        ['name' => 'Verde claro', 'color' => '#D9EAD3'],
+
+    ];
+
+    private $alliance = [
+
+        ['name' => '10'],
+        ['name' => '11'],
+        ['name' => '12'],
+        ['name' => '13'],
+        ['name' => '14'],
+        ['name' => '15'],
+        ['name' => '16'],
+        ['name' => '17'],
+        ['name' => '18'],
+        ['name' => '19'],
+        ['name' => '20'],
+        ['name' => '21'],
+        ['name' => '22'],
+        ['name' => '23'],
+        ['name' => '24'],
+        ['name' => '25'],
+        ['name' => '26'],
+        ['name' => '27'],
+        ['name' => '28'],
+        ['name' => '29'],
+        ['name' => '30'],
+        ['name' => '31'],
+        ['name' => '32'],
+        ['name' => '33'],
+        ['name' => '34'],
+        ['name' => '35'],
+        ['name' => '36'],
+        ['name' => '37'],
+        ['name' => '38'],
+        ['name' => '39'],
+        ['name' => '40'],
+
+    ];
+
+    private $pants = [
+
+        ['name' => '34'],
+        ['name' => '36'],
+        ['name' => '38'],
+        ['name' => '40'],
+        ['name' => '42'],
+        ['name' => '44'],
+        ['name' => '46'],
+        ['name' => '48'],
+        ['name' => '50'],
+        ['name' => '52'],
+        ['name' => '54'],
+        ['name' => '56'],
+        ['name' => '58'],
+        ['name' => '60'],
+
+    ];
+
+    private $shirts = [
+
+        ['name' => '1'],
+        ['name' => '2'],
+        ['name' => '3'],
+        ['name' => '4'],
+        ['name' => '5'],
+        ['name' => 'G'],
+        ['name' => 'GG'],
+        ['name' => 'M'],
+        ['name' => 'P'],
+        ['name' => 'PP'],
+        ['name' => 'U'],
+        ['name' => 'XG'],
+        ['name' => 'XGG'],
+
+    ];
+
+    private $sneakers = [
+
+        ['name' => '53-54'],
+        ['name' => '55-56'],
+        ['name' => '57-58'],
+        ['name' => '59-60'],
+        ['name' => '61-62'],
+        ['name' => '63-64'],
+        ['name' => '16'],
+        ['name' => '17'],
+        ['name' => '18'],
+        ['name' => '19'],
+        ['name' => '20'],
+        ['name' => '21'],
+        ['name' => '22'],
+        ['name' => '23'],
+        ['name' => '24'],
+        ['name' => '25'],
+        ['name' => '26'],
+        ['name' => '27'],
+        ['name' => '28'],
+        ['name' => '29'],
+        ['name' => '30'],
+        ['name' => '31'],
+        ['name' => '32'],
+        ['name' => '33'],
+        ['name' => '34'],
+        ['name' => '35'],
+        ['name' => '36'],
+        ['name' => '37'],
+        ['name' => '38'],
+        ['name' => '39'],
+        ['name' => '40'],
+        ['name' => '41'],
+        ['name' => '42'],
+        ['name' => '43'],
+        ['name' => '44'],
+        ['name' => '45'],
+        ['name' => '46'],
+        ['name' => '47'],
+
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -15,232 +189,77 @@ class AttributeSeeder extends Seeder
     public function run()
     {
 
-        foreach ($this->attributes() as $key => $attr) {
+        $this->genre();
+        $this->productOneColor();
+        $this->productTwoColors();
+        $this->sizeAlliance();
+        $this->sizePants();
+        $this->sizeShirts();
+        $this->sizeSneakers();
 
-            $attributes = Attribute::create($attr);
+    }
 
-            $attributes->each(function ($attribute) use ($attr) {
+    private function genre()
+    {
+        $attribute = Attribute::create($this->attributes[0]);
+        foreach ($this->genre as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        }          
+    }
 
-                foreach ($this->variations() as $key => $var) {
-
-                    $data = [
-                        'name' => $var['name'],
-                        'color' => isset( $var['color'] ) ? $var['color'] : null,
-                    ];
-
-                    if (is_null($data['color'])) {
-                        unset($data['color']);
-                    }
-
-                    if($attr[0] === $var[0]) {
-                        $variation = new AttributeVariation( $data );
-                        $attribute->variations()->save($variation);  
-                    }
-
-                }               
-
-            });
-
+    private function productOneColor()
+    {
+        $attribute = Attribute::create($this->attributes[1]);
+        foreach ($this->color as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
         }
-
     }
 
-
-    public function attributes()
+    private function productTwoColors()
     {
-        return [
-
-            ['1', 'name' => 'Gênero', 'type' => AttributeGroup::GENRE ],
-            ['2', 'name' => 'Produto com uma cor', 'type' => AttributeGroup::COLOR ],
-            ['3', 'name' => 'Produto com duas cores', 'type' => AttributeGroup::COLOR ],
-            ['4', 'name' => 'Tamanho de anel/aliança', 'type' => AttributeGroup::SIZE ],
-            ['5', 'name' => 'Tamanho de calça', 'type' => AttributeGroup::SIZE ],
-            ['6', 'name' => 'Tamanho de camisa/camiseta', 'type' => AttributeGroup::SIZE ],
-            ['7', 'name' => 'Tamanho de tênis', 'type' => AttributeGroup::SIZE ],
-
-        ];
-
+        $attribute = Attribute::create($this->attributes[2]);
+        foreach ($this->color as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        } 
     }
 
-
-    private function variations()
+    private function sizeAlliance()
     {
+        $attribute = Attribute::create($this->attributes[3]);
+        foreach ($this->alliance as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        } 
+    }
 
-        return [
+    private function sizePants()
+    {
+        $attribute = Attribute::create($this->attributes[4]);
+        foreach ($this->pants as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        } 
+    }
 
-            ['1', 'name' => 'Feminino'],
-            ['1', 'name' => 'Masculino'],
-            ['4', 'name' => '10'],
-            ['4', 'name' => '11'],
-            ['4', 'name' => '12'],
-            ['4', 'name' => '13'],
-            ['4', 'name' => '14'],
-            ['4', 'name' => '15'],
-            ['4', 'name' => '16'],
-            ['4', 'name' => '17'],
-            ['4', 'name' => '18'],
-            ['4', 'name' => '19'],
-            ['4', 'name' => '20'],
-            ['4', 'name' => '21'],
-            ['4', 'name' => '22'],
-            ['4', 'name' => '23'],
-            ['4', 'name' => '24'],
-            ['4', 'name' => '25'],
-            ['4', 'name' => '26'],
-            ['4', 'name' => '27'],
-            ['4', 'name' => '28'],
-            ['4', 'name' => '29'],
-            ['4', 'name' => '30'],
-            ['4', 'name' => '31'],
-            ['4', 'name' => '32'],
-            ['4', 'name' => '33'],
-            ['4', 'name' => '34'],
-            ['4', 'name' => '35'],
-            ['4', 'name' => '36'],
-            ['4', 'name' => '37'],
-            ['4', 'name' => '38'],
-            ['4', 'name' => '39'],
-            ['4', 'name' => '40'],
-            ['5', 'name' => '34'],
-            ['5', 'name' => '36'],
-            ['5', 'name' => '38'],
-            ['5', 'name' => '40'],
-            ['5', 'name' => '42'],
-            ['5', 'name' => '44'],
-            ['5', 'name' => '46'],
-            ['5', 'name' => '48'],
-            ['5', 'name' => '50'],
-            ['5', 'name' => '52'],
-            ['5', 'name' => '54'],
-            ['5', 'name' => '56'],
-            ['5', 'name' => '58'],
-            ['5', 'name' => '60'],
-            ['6', 'name' => '1'],
-            ['6', 'name' => '2'],
-            ['6', 'name' => '3'],
-            ['6', 'name' => '4'],
-            ['6', 'name' => '5'],
-            ['6', 'name' => 'G'],
-            ['6', 'name' => 'GG'],
-            ['6', 'name' => 'M'],
-            ['6', 'name' => 'P'],
-            ['6', 'name' => 'PP'],
-            ['6', 'name' => 'U'],
-            ['6', 'name' => 'XG'],
-            ['6', 'name' => 'XGG'],
-            ['7', 'name' => '53-54'],
-            ['7', 'name' => '55-56'],
-            ['7', 'name' => '57-58'],
-            ['7', 'name' => '59-60'],
-            ['7', 'name' => '61-62'],
-            ['7', 'name' => '63-64'],
-            ['7', 'name' => '16'],
-            ['7', 'name' => '17'],
-            ['7', 'name' => '18'],
-            ['7', 'name' => '19'],
-            ['7', 'name' => '20'],
-            ['7', 'name' => '21'],
-            ['7', 'name' => '22'],
-            ['7', 'name' => '23'],
-            ['7', 'name' => '24'],
-            ['7', 'name' => '25'],
-            ['7', 'name' => '26'],
-            ['7', 'name' => '27'],
-            ['7', 'name' => '28'],
-            ['7', 'name' => '29'],
-            ['7', 'name' => '30'],
-            ['7', 'name' => '31'],
-            ['7', 'name' => '32'],
-            ['7', 'name' => '33'],
-            ['7', 'name' => '34'],
-            ['7', 'name' => '35'],
-            ['7', 'name' => '36'],
-            ['7', 'name' => '37'],
-            ['7', 'name' => '38'],
-            ['7', 'name' => '39'],
-            ['7', 'name' => '40'],
-            ['7', 'name' => '41'],
-            ['7', 'name' => '42'],
-            ['7', 'name' => '43'],
-            ['7', 'name' => '44'],
-            ['7', 'name' => '45'],
-            ['7', 'name' => '46'],
-            ['7', 'name' => '47'],
-            ['2', 'name' => 'Creme', 'color' => '#FFF2CC'],
-            ['2', 'name' => 'Marrom', 'color' => '#B45F06'],
-            ['2', 'name' => 'Vermelho', 'color' => '#FF0000'],
-            ['2', 'name' => 'Cinza Claro', 'color' => '#CCCCCC'],
-            ['2', 'name' => 'Púrpura', 'color' => '#741B47'],
-            ['2', 'name' => 'Laranja', 'color' => '#FF9900'],
-            ['2', 'name' => 'Fúcsia', 'color' => '#E828FF'],
-            ['2', 'name' => 'Lilás', 'color' => '#8E7CC3'],
-            ['2', 'name' => 'Marrom amarelado', 'color' => '#F6B26B'],
-            ['2', 'name' => 'Ciano escuro', 'color' => '#76A5AF'],
-            ['2', 'name' => 'Verde escuro', 'color' => '#38761D'],
-            ['2', 'name' => 'Rosa', 'color' => '#F4CCCC'],
-            ['2', 'name' => 'Esmeralda', 'color' => '#93C47D'],
-            ['2', 'name' => 'Salmão', 'color' => '#EAD1DC'],
-            ['2', 'name' => 'Verde', 'color' => '#0C9800'],
-            ['2', 'name' => 'Fúcsia escuro', 'color' => '#C27BA0'],
-            ['2', 'name' => 'Dourado', 'color' => '#BF9000'],
-            ['2', 'name' => 'Lavanda', 'color' => '#D9D2E9'],
-            ['2', 'name' => 'Água', 'color' => '#83DDFF'],
-            ['2', 'name' => 'Azul Céu', 'color' => '#D0E0E3'],
-            ['2', 'name' => 'Preto', 'color' => '#000000'],
-            ['2', 'name' => 'Azul', 'color' => '#1717FF'],
-            ['2', 'name' => 'Azul claro', 'color' => '#CFE2F3'],
-            ['2', 'name' => 'Azul aço', 'color' => '#6FA8DC'],
-            ['2', 'name' => 'Vermelho escuro', 'color' => '#990000'],
-            ['2', 'name' => 'Azul escuro', 'color' => '#0B5394'],
-            ['2', 'name' => 'Índigo', 'color' => '#351C75'],
-            ['2', 'name' => 'Branco', 'color' => '#FFFFFF'],
-            ['2', 'name' => 'Branco Navajo', 'color' => '#FCE5CD'],
-            ['2', 'name' => 'Mocassim', 'color' => '#FFD966'],
-            ['2', 'name' => 'Azul petróleo', 'color' => '#134F5C'],
-            ['2', 'name' => 'Amarelo', 'color' => '#FFFF00'],
-            ['2', 'name' => 'Terracota', 'color' => '#E06666'],
-            ['2', 'name' => 'Violeta escuro', 'color' => '#7600FF'],
-            ['2', 'name' => 'Cinza', 'color' => '#666666'],
-            ['2', 'name' => 'Verde claro', 'color' => '#D9EAD3'],
-            ['3', 'name' => 'Creme', 'color' => '#FFF2CC'],
-            ['3', 'name' => 'Marrom', 'color' => '#B45F06'],
-            ['3', 'name' => 'Vermelho', 'color' => '#FF0000'],
-            ['3', 'name' => 'Cinza Claro', 'color' => '#CCCCCC'],
-            ['3', 'name' => 'Púrpura', 'color' => '#741B47'],
-            ['3', 'name' => 'Laranja', 'color' => '#FF9900'],
-            ['3', 'name' => 'Fúcsia', 'color' => '#E828FF'],
-            ['3', 'name' => 'Lilás', 'color' => '#8E7CC3'],
-            ['3', 'name' => 'Marrom amarelado', 'color' => '#F6B26B'],
-            ['3', 'name' => 'Ciano escuro', 'color' => '#76A5AF'],
-            ['3', 'name' => 'Verde escuro', 'color' => '#38761D'],
-            ['3', 'name' => 'Rosa', 'color' => '#F4CCCC'],
-            ['3', 'name' => 'Esmeralda', 'color' => '#93C47D'],
-            ['3', 'name' => 'Salmão', 'color' => '#EAD1DC'],
-            ['3', 'name' => 'Verde', 'color' => '#0C9800'],
-            ['3', 'name' => 'Fúcsia escuro', 'color' => '#C27BA0'],
-            ['3', 'name' => 'Dourado', 'color' => '#BF9000'],
-            ['3', 'name' => 'Lavanda', 'color' => '#D9D2E9'],
-            ['3', 'name' => 'Água', 'color' => '#83DDFF'],
-            ['3', 'name' => 'Azul Céu', 'color' => '#D0E0E3'],
-            ['3', 'name' => 'Preto', 'color' => '#000000'],
-            ['3', 'name' => 'Azul', 'color' => '#1717FF'],
-            ['3', 'name' => 'Azul claro', 'color' => '#CFE2F3'],
-            ['3', 'name' => 'Azul aço', 'color' => '#6FA8DC'],
-            ['3', 'name' => 'Vermelho escuro', 'color' => '#990000'],
-            ['3', 'name' => 'Azul escuro', 'color' => '#0B5394'],
-            ['3', 'name' => 'Índigo', 'color' => '#351C75'],
-            ['3', 'name' => 'Branco', 'color' => '#FFFFFF'],
-            ['3', 'name' => 'Branco Navajo', 'color' => '#FCE5CD'],
-            ['3', 'name' => 'Mocassim', 'color' => '#FFD966'],
-            ['3', 'name' => 'Azul petróleo', 'color' => '#134F5C'],
-            ['3', 'name' => 'Amarelo', 'color' => '#FFFF00'],
-            ['3', 'name' => 'Terracota', 'color' => '#E06666'],
-            ['3', 'name' => 'Violeta escuro', 'color' => '#7600FF'],
-            ['3', 'name' => 'Cinza', 'color' => '#666666'],
-            ['3', 'name' => 'Verde claro', 'color' => '#D9EAD3'],
+    private function sizeShirts()
+    {
+        $attribute = Attribute::create($this->attributes[5]);
+        foreach ($this->shirts as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        } 
+    }
 
-        ];    
-
+    private function sizeSneakers()
+    {
+        $attribute = Attribute::create($this->attributes[6]);
+        foreach ($this->sneakers as $key => $value) {
+            $variation = new AttributeVariation( $value );
+            $attribute->variations()->save($variation);
+        } 
     }
 
 }
